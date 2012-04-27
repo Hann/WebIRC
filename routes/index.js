92edc71496b41397f381b098417b38b4ca2f041a
Jinsoo Han <launchinghann@facebook.com>
2012-04-27 23:58:42 +0900

    css

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,12 @@
 
 // login function
 exports.index = function(req, res){
-  res.render('index', { title: 'Welcome To WebIRC!' });
+  res.render('index', { css: 'index', title: 'Welcome To WebIRC!' });
 };
 
 // chat function
 exports.chat= function(req, res){
-  res.render('chat', { title: 'connect IRC',
+  res.render('chat', { css:'chat', title: 'connect IRC',
 		     nickname : req.body.nickname,
 		     channel : req.body.channelName });
 };
