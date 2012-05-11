4d9b86a149073d7c89223037478e5005ade32af6
Jinsoo Han <launchinghann@facebook.com>
2012-05-11 14:06:38 +0900

    added TabManager.js and disappear PING message

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,63 +1,84 @@
 $(document).ready(function () {
-var input = $('message');
-var Message = IRCPacket.Message;
-var Prefix = IRCPacket.Prefix;
-var socket = io.connect('http://hann.iptime.org');
-var time = $('#time');
-var log = $('#log');
-
-function appendlog(message){
-    log.append("<p>" + message + "<p>");
-    time.append("<p>" + (new Date().toLocaleTimeString()) + "</p>");
-}
-
-function emit(message){
-    socket.emit('relay', message);
-}
-
-socket.on('ready', function(ip){
-	      var message = new Message('NICK' , nickname).build();
-	      emit(message);
-	      message = new Message('USER', nickname, ip, 'chat.freenode.net', nickname).build();
-	      emit(message);
-	      appendLog(message);
-
-	  });
-
-socket.on('relay', function(message){
-	     appendLog(message); 
-	  });
-
+		      var input = $('#message');
+		      var Message = IRCPacket.Message;
+		      var Prefix = IRCPacket.Prefix;
+		      var socket = io.connect('http://hann.iptime.org');
+		      var time = $('#time');
+		      var log = $('#log');
+		      var scrollHeight;
 		      
-$(function(){
-      $(window).resize(function(){
-			   width = parseInt($(this).width());
-			   height = parseInt($(this).height());
-			   $('#chat-container').height(height-140);
-			   $('#chat-container').width(width-140);
-		       }).resize();
-  });
+		      function appendLog(message){
+			  log.append("<p>" + message + "</p>");
+			  time.append("<p>" + (new Date().toLocaleTimeString()) + "</p>");
+		      }
 
-// use jQuery.
-input.keyup(function (event){
-    if (event.which == 13){
-	var message = input.val();
-	console.log(message);
-	if (message != ''){
-	    var cr = new CommandReader();
-	    var data = cr.parseText(message);
-	    if (data.command == "PRIVMSG"){
-		var log = "<p>"+ nickname + " : " +  data.parameters +"</p>";
-		var time = "<p>" + (new Date().toLocaleTimeString()) + "</p>";
-		$('#log').append(log);
-		$('#time').append(time);
-		emit(data.command, data.meters);
-	    }
+		      function emit(message){
+			  console.log('emit : ' + message);
+			  socket.emit('relay', message);
+		      }
+		      function scroll(){
+			  scrollHeight += 20;
+			  console.log(scrollHeight);
+			  $('#chat-container').scrollTop(scrollHeight);
+		      }
+		      
+		      socket.on('ready', function(ip){
+				    var message = new Message('NICK' , nickname).build();			
+				    emit(message);
+				    
+				    message = new Message('USER', nickname, ip, 'chat.freenode.net', nickname).build();
+				    console.log("NICK ",message);
+				    emit(message);
+				    appendLog(message);
+				    
+				    message = new Message('JOIN', channel).build();
+				    emit(message);
+				    appendLog(message);
+				});
 
-	    input.val('');
-	    $('#chat-container').scrollTop(height);
-	}
-    }
-    
-});
-});
+		      socket.on('relay', function(message){
+				    var packet = new Message().parse(message);
+				    if (packet.command === 'PING'){
+					packet.command = 'PONG';
+					packet = packet.build();
+					emit(packet);
+				    }
+				    else{
+					appendLog(message);
+					scroll();
+				    }
+				});
+
+
+		      $(function(){
+			    $(window).resize(function(){
+						 width = parseInt($(this).width());
+						 height = parseInt($(this).height());
+						 scrollHeight = height;
+						 $('#chat-container').height(height-140);
+						 $('#chat-container').width(width-140);
+					     }).resize();
+			});
+
+		      // use jQuery.
+		      input.keyup(function (event){
+				      if (event.which == 13){
+					  var message = input.val();
+					  console.log(message);
+					  if (message != ''){
+					      var cr = new CommandReader();
+					      var data = cr.parseText(message);
+					      
+					      if (data.command == "PRIVMSG"){
+						  appendLog(message);
+
+					      }
+					      message = new Message().parse(data.command + " " + data.parameters).build();
+					      emit(message);
+					      input.val('');
+					      
+					      scroll();
+					  }
+				      }
+				  });
+		  });
