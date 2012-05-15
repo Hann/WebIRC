cfe0a5c8da044664fca221fe4e03323f291c6432
Jonghwan Hyeon <hyeon0135@gmail.com>
2012-05-15 17:25:17 +0900

    groundwork for creating Notifier

---
+++ public/javascripts/Notifier.js
@@ -0,0 +1,18 @@
+/*
+  1. check that the current browser supports Notification
+  2. reuqest permission
+  3. create notification -> normal or html
+    - normal
+      - image url
+      - title
+      - content
+      - alarm
+    - html
+      -url that you want to show a page
+  4. close the notification (optional)
+
+  Reference: http://beakkon.com/tutorial/html5/desktop-notification
+*/
+var Notifier = function () {
+
+};
